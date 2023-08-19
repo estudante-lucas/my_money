0a33d4d07294945c5f6f5e101df59d7cda2fbaf1
Guga <lucas.hillesheim@paytrack.com.br>
2023-08-19 15:32:23 -0300

    Adicionado componentes compartilhados

---
+++ lib/shared/components/app_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final VoidCallback action;
+  final BuildContext? context;
+  final String label;
+  final double? paddingTop;
+  final double? paddingBottom;
+  final double? paddingLeft;
+  final double? paddingRigth;
+
+  final double? height;
+
+  const AppButton({
+    Key? key,
+    required this.action,
+    this.context,
+    required this.label,
+    this.paddingTop,
+    this.paddingBottom,
+    this.paddingLeft,
+    this.paddingRigth,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: paddingBottom ?? 0,
+        top: paddingTop ?? 0,
+        left: paddingLeft ?? 0,
+        right: paddingRigth ?? 0,
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        child: TextButton(
+          onPressed: action,
+          style: TextButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.green,
+            padding: const EdgeInsets.all(12),
+            surfaceTintColor: Colors.green,
+          ),
+          child: Text(label,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.clip,
+              style: const TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+}
