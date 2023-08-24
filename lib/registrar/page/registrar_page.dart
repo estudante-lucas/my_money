c890ecda471360e430a496d49c51254a11df0901
Guga <lucas.hillesheim@paytrack.com.br>
2023-08-23 22:39:31 -0300

    Criado tela de cadastro de usuário

---
+++ lib/registrar/page/registrar_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:my_money/shared/colors/app_colors.dart';
+import 'package:my_money/shared/components/app_button.dart';
+import 'package:my_money/shared/components/app_logo_title.dart';
+
+class RegistrarPage extends StatefulWidget {
+  const RegistrarPage({super.key});
+
+  @override
+  State<RegistrarPage> createState() => _RegistrarPageState();
+}
+
+class _RegistrarPageState extends State<RegistrarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, left: 25, right: 25),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const AppLogoTitle(
+                    title: 'Cadastro My Money',
+                    titleSize: 20,
+                    iconSize: 20,
+                  ),
+                  TextFormField(
+                    autofocus: false,
+                    enabled: true,
+                    keyboardType: TextInputType.text,
+                    textAlign: TextAlign.start,
+                    decoration: const InputDecoration(
+                      label: Text("Nome Completo"),
+                    ),
+                  ),
+                  TextFormField(
+                    autofocus: false,
+                    enabled: true,
+                    keyboardType: TextInputType.text,
+                    textAlign: TextAlign.start,
+                    decoration: const InputDecoration(
+                      label: Text("E-mail"),
+                    ),
+                  ),
+                  TextFormField(
+                    autofocus: false,
+                    enabled: true,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    textAlign: TextAlign.start,
+                    decoration: const InputDecoration(
+                      label: Text("Senha"),
+                    ),
+                  ),
+                  TextFormField(
+                    autofocus: false,
+                    enabled: true,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    textAlign: TextAlign.start,
+                    decoration: const InputDecoration(
+                      label: Text("Confirmar senha"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: AppButton(
+                      action: () {},
+                      label: "Cadastrar",
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
