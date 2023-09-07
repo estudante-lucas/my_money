5ff4c953d1cf7fd0cf04d9a675d5d19c57dc6716
Guga <lucas.hillesheim@paytrack.com.br>
2023-09-07 17:24:47 -0300

    Refatorado home page

--- lib/home/page/home_page.dart
+++ lib/home/page/home_page.dart
@@ -4,8 +4,10 @@ import 'package:my_money/home/components/expense_button.dart';
 import 'package:my_money/home/components/expense_count.dart';
 import 'package:my_money/home/components/management_indicator.dart';
 import 'package:my_money/home/controller/home_controller.dart';
+import 'package:my_money/router/app_router.dart';
 import 'package:my_money/shared/colors/app_colors.dart';
 import 'package:my_money/shared/components/app_button.dart';
+import 'package:my_money/shared/components/app_loading.dart';
 import 'package:my_money/shared/components/app_title.dart';
 import 'package:my_money/shared/components/expense_list.dart';
 import 'package:my_money/shared/metrics/app_metrics.dart';
@@ -28,142 +30,151 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (_) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.background,
-          centerTitle: true,
-          toolbarHeight: AppMetrics.barHeight,
-          elevation: 0,
-          actions: [
-            IconButton(
-              onPressed: () => controller.logout(context),
-              icon: const Icon(Icons.logout),
-            )
-          ],
-          leading: IconButton(
-            onPressed: () => controller.goSettings(context),
-            icon: const Icon(Icons.settings_outlined),
-          ),
-        ),
-        backgroundColor: AppColors.background,
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 25,
-                  right: 25,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const AppTitle(title: "Dashboard MyMoney"),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          ExpenseCount(
-                            title: "Gasto acumulado",
-                            value: controller.accValue,
-                            background: AppColors.error,
-                            icon: Icons.trending_up,
-                          ),
-                          ExpenseCount(
-                            title: "Gasto planejado",
-                            value: controller.goalValue,
-                            background: Colors.lightGreen,
-                            icon: Icons.trending_up,
-                          )
-                        ],
+    return Observer(
+      builder: (_) {
+        return controller.isLoading
+            ? const AppLoading()
+            : Scaffold(
+                appBar: AppBar(
+                  backgroundColor: AppColors.background,
+                  centerTitle: true,
+                  toolbarHeight: AppMetrics.barHeight,
+                  elevation: 0,
+                  actions: [
+                    IconButton(
+                      onPressed: () => controller.logout(context),
+                      icon: const Icon(
+                        Icons.logout,
+                        color: AppColors.logo,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 10, top: 10),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          color: AppColors.background,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 20),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: const [
-                                    ManagementIndicator(
-                                      value: 1762,
-                                      subtitle: "Saldo gasto planejado",
-                                      minValueAxis: 0,
-                                      maxValueAxis: 2000,
-                                      isASC: false,
-                                    ),
-                                    ManagementIndicator(
-                                      value: 70,
-                                      subtitle: "Saldo despesa diária",
-                                      minValueAxis: 0,
-                                      maxValueAxis: 150,
-                                      isASC: false,
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 20),
-                                child: Row(
-                                  children: const [
-                                    ManagementIndicator(
-                                      value: 10,
-                                      subtitle: "Período decorrido",
-                                      minValueAxis: 1,
-                                      maxValueAxis: 30,
-                                      isMoney: false,
-                                    ),
-                                    ManagementIndicator(
-                                      value: 20,
-                                      subtitle: "Saldo despesa diária",
-                                      minValueAxis: 0,
-                                      maxValueAxis: 150,
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: ExpenseList(
-                        expenses: controller.expenses,
-                        title: "Gastos recentes",
-                      ),
-                    ),
-                    AppButton(
-                      action: () {},
-                      label: "Histórico",
-                    )
                   ],
+                  leading: IconButton(
+                      onPressed: () => controller.goSettings(context),
+                      icon: const Icon(
+                        Icons.settings_outlined,
+                        color: AppColors.logo,
+                      )),
                 ),
-              ),
-            ),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          child: Container(
-            height: 40,
-          ),
-        ),
-        floatingActionButton: ExpenseButton.add(context),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      );
-    });
+                backgroundColor: AppColors.background,
+                body: SingleChildScrollView(
+                  child: SafeArea(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 25,
+                          right: 25,
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const AppTitle(title: "Dashboard My Money"),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  ExpenseCount(
+                                    title: "Gasto acumulado",
+                                    value: controller.accValue,
+                                    background: Colors.redAccent,
+                                    icon: Icons.trending_down_rounded,
+                                  ),
+                                  ExpenseCount(
+                                    title: "Gasto Planejado",
+                                    value: controller.goalValue,
+                                    background: Colors.lightGreen,
+                                    icon: Icons.trending_up,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20, top: 10),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(color: const Color(0xFFE0E0E0), borderRadius: BorderRadius.circular(10)),
+                                child: Center(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(vertical: 20.0),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                          children: [
+                                            ManagementIndicator(
+                                              value: controller.plannedSpentBalance,
+                                              subtitle: "Saldo gasto planejado",
+                                              minValueAxis: 0,
+                                              maxValueAxis: controller.goalValue,
+                                              isASC: false,
+                                            ),
+                                            ManagementIndicator(
+                                              value: controller.dailyExpenseBalance,
+                                              subtitle: "Saldo despesa diária",
+                                              minValueAxis: 0,
+                                              maxValueAxis: controller.dailyExpenseBalance,
+                                              isASC: false,
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(vertical: 20.0),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                          children: [
+                                            ManagementIndicator(
+                                              value: controller.dayOfMonth.toDouble(),
+                                              subtitle: "Período decorrido",
+                                              minValueAxis: 1,
+                                              maxValueAxis: 30,
+                                              isMoney: false,
+                                            ),
+                                            ManagementIndicator(
+                                              value: controller.expensesDay,
+                                              subtitle: "Despesas do dia",
+                                              minValueAxis: 0,
+                                              maxValueAxis: controller.dailyExpenseBalance,
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: ExpenseList(
+                                expenses: controller.expenses,
+                                title: "Gastos recentes",
+                              ),
+                            ),
+                            AppButton(
+                              action: () {
+                                Navigator.of(context).pushNamed(AppRouter.history, arguments: {
+                                  "expenses": controller.expenseList,
+                                });
+                              },
+                              label: "Histórico",
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                bottomNavigationBar: BottomAppBar(
+                  shape: const CircularNotchedRectangle(),
+                  child: Container(height: 40),
+                ),
+                floatingActionButton: ExpenseButton.add(context),
+                floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+              );
+      },
+    );
   }
 }
