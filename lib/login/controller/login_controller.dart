6c7b98ce6404ced7ee5630652cc36670d4c69bc1
Guga <lucas.hillesheim@paytrack.com.br>
2023-09-07 17:04:54 -0300

    Adicionado App Snackbar

--- lib/login/controller/login_controller.dart
+++ lib/login/controller/login_controller.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:my_money/login/service/login_service.dart';
+import 'package:my_money/shared/components/app_snackbar.dart';
 import 'package:my_money/shared/helpers/validator.dart';
+
 part 'login_controller.g.dart';
 
 class LoginController = LoginControllerBase with _$LoginController;
@@ -34,7 +36,10 @@ abstract class LoginControllerBase with Store {
       await sendData();
       isLoading = false;
     } else {
-      SnackBarAction(label: "Verifique os dados preenchidos", onPressed: () {});
+      AppSnackbar.openMessage(
+        context: buildContext,
+        message: "Verifique os dados preenchidos",
+      );
     }
   }
 
@@ -59,14 +64,22 @@ abstract class LoginControllerBase with Store {
   void getException(int code) {
     switch (code) {
       case 401:
-        SnackBarAction(label: "Acesso não autorizado!", onPressed: () {});
+        AppSnackbar.openMessage(
+          context: buildContext,
+          message: "Acesso não autorizado",
+        );
         break;
       case 400:
-        SnackBarAction(label: "Dados inválidos!", onPressed: () {});
+        AppSnackbar.openMessage(
+          context: buildContext,
+          message: "Dados inválidos",
+        );
         break;
       default:
-        SnackBarAction(label: "Erro inesperdo! Tente mais tarde.", onPressed: () {});
-        break;
+        AppSnackbar.openMessage(
+          context: buildContext,
+          message: "Erro inesperado, tente mais tarde",
+        );
     }
   }
 }
