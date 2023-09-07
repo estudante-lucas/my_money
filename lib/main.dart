bbca50b866722c2c2ce445358a57972ca1de8389
Guga <lucas.hillesheim@paytrack.com.br>
2023-09-07 17:42:20 -0300

    Ajustado as rotas do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_money/history/history_page.dart';
+import 'package:my_money/home/page/home_page.dart';
+import 'package:my_money/initial/page/initial_page.dart';
 import 'package:my_money/login/page/login_page.dart';
+import 'package:my_money/personal_register/page/personal_register_page.dart';
+import 'package:my_money/registrar/page/registrar_page.dart';
+import 'package:my_money/router/app_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyMoney',
+      title: 'My Money',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const LoginPage(),
+      home: const InitialPage(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      routes: {
+        AppRouter.login: (context) => const LoginPage(),
+        AppRouter.home: (context) => const HomePage(),
+        AppRouter.history: (context) => const HistoryPage(),
+        AppRouter.register: (context) => const RegistrarPage(),
+        AppRouter.personalRegister: (context) => const PersonalRegisterPage(),
+      },
     );
   }
 }
