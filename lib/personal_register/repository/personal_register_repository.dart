c7352515fe67423d5be9398b6a4cc18397d51b94
Guga <lucas.hillesheim@paytrack.com.br>
2023-09-09 23:03:07 -0300

    Adicionado controller, repository e service

---
+++ lib/personal_register/repository/personal_register_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:my_money/http/app_dio.dart';
+import 'package:my_money/personal_register/model/personal_register_model.dart';
+import 'package:my_money/shared/helpers/constants.dart';
+import 'package:my_money/shared/model/user_update_model.dart';
+
+class PersonalRegisterRepository {
+  Future<Response<Map<String, dynamic>>> sendData(PersonalRegisterModel personalRegisterData) async {
+    String url = "http://${Constants.hostname}:3099/goal/create";
+
+    Dio clientHTTP = await AppDio.getConnection(isAuth: true);
+
+    return clientHTTP.post(url, data: personalRegisterData.toJson());
+  }
+
+  Future<Response<Map<String, dynamic>>> updateDataUser(String userId, UserUpdateModel userUpdate) async {
+    String url = "http://${Constants.hostname}:3099/user/update/$userId";
+
+    Dio clientHTTP = await AppDio.getConnection(isAuth: true);
+
+    return clientHTTP.patch(url, data: userUpdate.toJson());
+  }
+}
---
+++ lib/personal_register/service/personal_register_service.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:my_money/personal_register/model/personal_register_model.dart';
+import 'package:my_money/personal_register/repository/personal_register_repository.dart';
+import 'package:my_money/shared/helpers/authenticated_user.dart';
+import 'package:my_money/shared/model/user_model.dart';
+import 'package:my_money/shared/model/user_update_model.dart';
+
+class PersonalRegisterService {
+  PersonalRegisterRepository repository = PersonalRegisterRepository();
+
+  Future<bool> sendData({
+    required String name,
+    required String email,
+    required double limitValue,
+  }) async {
+    try {
+      UserModel user = await AuthenticatedUser.getUserData();
+
+      if (name.compareTo(user.fullName) != 0 || email.compareTo(user.email) != 0) {
+        await updateRegister(
+          user.id,
+          UserUpdateModel(
+            fullName: name,
+            email: email,
+          ),
+        );
+      }
+
+      if (limitValue != user.limitValue) {
+        await sendPersonalRegister(limitValue, user.id);
+      }
+
+      return true;
+    } catch (exception) {
+      debugPrint(exception.toString());
+      return false;
+    }
+  }
+
+  Future<void> sendPersonalRegister(double limitValue, String userId) async {
+    await repository.sendData(PersonalRegisterModel(
+      userId: userId,
+      limitValue: limitValue,
+    ));
+  }
+
+  Future<void> updateRegister(String userId, UserUpdateModel userUpdate) async {
+    await repository.updateDataUser(userId, userUpdate);
+  }
+}
