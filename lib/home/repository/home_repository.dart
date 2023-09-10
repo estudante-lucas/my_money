625d546b12f95c4b2ba33dfa484f49e9b0b79e19
Guga <lucas.hillesheim@paytrack.com.br>
2023-09-09 23:04:25 -0300

    Ajustado para usar constants

--- lib/home/repository/home_repository.dart
+++ lib/home/repository/home_repository.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:my_money/http/app_dio.dart';
+import 'package:my_money/shared/helpers/constants.dart';
 
 class HomeRepository {
   Future<Response<List<dynamic>>> getExpenses(String userId) async {
-    String url = "http://localhost:3099/expense/findAllByUser/$userId";
+    String url = "http://${Constants.hostname}:3099/expense/findAllByUser/$userId";
 
     Dio clientHTTP = await AppDio.getConnection(isAuth: true);
 
@@ -11,7 +12,7 @@ class HomeRepository {
   }
 
   Future<Response<Map<String, dynamic>>> getGoal(String userId) async {
-    String url = "http://localhost:3099/goal/findByUser/$userId";
+    String url = "http://${Constants.hostname}:3099/goal/findByUser/$userId";
 
     Dio clientHTTP = await AppDio.getConnection(isAuth: true);
 
--- lib/login/repository/login_repository.dart
+++ lib/login/repository/login_repository.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:my_money/http/app_dio.dart';
 import 'package:my_money/login/model/login_model.dart';
+import 'package:my_money/shared/helpers/constants.dart';
 
 class LoginRepository {
-  String baseUrl = "http://localhost:3099/auth/login";
+  String baseUrl = "http://${Constants.hostname}:3099/auth/login";
 
   Future<Response<Map<String, dynamic>>> sendData(LoginModel model) async {
     Dio clientHttp = await AppDio.getConnection(isAuth: false);
